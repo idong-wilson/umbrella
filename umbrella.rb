7468fde7d542c15155ceaf9abcdb829e8555abef
idong-wilson <45530856+idong-wilson@users.noreply.github.com>
2024-03-25 07:56:23 +0000

    Updated the hourly forecast

--- umbrella.rb
+++ umbrella.rb
@@ -1,65 +1,120 @@
-puts "Hi!"
-
-puts "What is your name?"
-
-user_name = gets.chomp
-
-puts "#{user_name}, Where are you?"
-
+puts "Hello!"
+puts "Where are you located?"
+# user_location = "Atlanta" # Manually set the location
 user_location = gets.chomp
-
-# user_location = "Atlanta"
-
-# Fetch latitude and longitude using google maps API
-
-maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+puts user_location
 
 require "http"
-
-resp = HTTP.get(maps_url)
-
-raw_response = resp.to_s
-
 require "json"
+require "time"
 
-parsed_response = JSON.parse(raw_response)
+# Fetch latitude and longitude using google maps API
+maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_location}&key=#{ENV.fetch('GMAPS_KEY')}"
+resp = HTTP.get(maps_url)
+parsed_response = JSON.parse(resp)
+location = parsed_response.fetch("results").at(0).fetch("geometry").fetch("location")
 
-results = parsed_response.fetch("results")
-
-first_result = results.at(0)
-
-geo = first_result.fetch("geometry")
-
-loc = geo.fetch("location")
-
-latitude = loc.fetch("lat")
-longitude = loc.fetch("lng")
-
+latitude = location.fetch("lat")
+longitude = location.fetch("lng")
+puts "Geographical coordinates of #{user_location}: #{latitude}, #{longitude}"
 
 # Assemble URL for the Pirate Weather API
-
 pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch('PIRATE_WEATHER_KEY')}/#{latitude},#{longitude}"
-
-# From the Pirate Weather API, fetch weather info
-
+# Fetch weather info
 resp = HTTP.get(pirate_weather_url)
-
 weather_info = JSON.parse(resp)
 
 # Fetch the current temperature
-
 current_temp = weather_info["currently"]["temperature"]
+puts "The current temperature in #{user_location} is #{current_temp}°F"
 
-puts "The temperature at #{user_location} is currently #{current_temp}°F."
-
-# Show the summary of the weather for the next hour
-require "time"
-
+# Display the summary of the weather for the next hour
 next_hour_summary = weather_info["hourly"]["summary"]
+puts "Weather summary for the next hour: #{next_hour_summary}"
 
-puts "For the next hour, the sky will be: #{next_hour_summary}"
+# Check precipitation probability for the next twelve hours
+umbrella_needed = false
+for i in 1..12
+  hour_info = weather_info["hourly"]["data"][i]
+  precip_probability = hour_info["precipProbability"]
+  if precip_probability > 0.1
+    umbrella_needed = true
+    puts "In the next #{i} hour(s), there is a #{(precip_probability * 100).round}% chance of precipitation."
+  end
+end
 
-#Check for precipitation level for the next 12 hours
+if umbrella_needed
+  puts "You might want to carry an umbrella!"
+else
+  puts "You probably won't need an umbrella today."
+end
+
+
+
+
+
+# puts "Hi!"
+
+# puts "What is your name?"
+
+# user_name = gets.chomp
+
+# puts "#{user_name}, Where are you?"
+
+# user_location = gets.chomp
+
+# # user_location = "Atlanta"
+
+# # Fetch latitude and longitude using google maps API
+
+# maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
+
+# require "http"
+
+# resp = HTTP.get(maps_url)
+
+# raw_response = resp.to_s
+
+# require "json"
+
+# parsed_response = JSON.parse(raw_response)
+
+# results = parsed_response.fetch("results")
+
+# first_result = results.at(0)
+
+# geo = first_result.fetch("geometry")
+
+# loc = geo.fetch("location")
+
+# latitude = loc.fetch("lat")
+# longitude = loc.fetch("lng")
+
+
+# # Assemble URL for the Pirate Weather API
+
+# pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch('PIRATE_WEATHER_KEY')}/#{latitude},#{longitude}"
+
+# # From the Pirate Weather API, fetch weather info
+
+# resp = HTTP.get(pirate_weather_url)
+
+# weather_info = JSON.parse(resp)
+
+# # Fetch the current temperature
+
+# current_temp = weather_info["currently"]["temperature"]
+
+# puts "The temperature at #{user_location} is currently #{current_temp}°F."
+
+# # Show the summary of the weather for the next hour
+# require "time"
+
+# next_hour_summary = weather_info["hourly"]["summary"]
+
+# puts "For the next hour, the sky will be: #{next_hour_summary}"
+
+# #Check for precipitation level for the next 12 hours
 
 # # Get the current hour in local time
 # current_hour = Time.now.hour
@@ -77,36 +132,6 @@ puts "For the next hour, the sky will be: #{next_hour_summary}"
 # end
 
 
-
-
-umbrella_needed = false
-for i in 0..11
-
-  hour_info = weather_info["hourly"]["data"][i]
-
-  hour_time = Time.at(hour_info["time"]).strftime("%l:%M %P")
-
-  precip_probability = hour_info["precipProbability"]
-
-  if precip_probability > 0.1
-
-    umbrella_needed = true
-
-    puts "In #{hour_time}, there's a #{(precip_probability * 100).round}% chance of precipitation."
-
-  end
-
-end
-
-if umbrella_needed
-
-  puts "You might want to carry an umbrella!"
-
-else
-
-  puts "You probably won't need an umbrella today."
-  
-end
 
 
 
