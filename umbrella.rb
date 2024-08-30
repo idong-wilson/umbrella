c67f340e8e4d1d94280efdf2e04a96feb0202252
idong-wilson <45530856+idong-wilson@users.noreply.github.com>
2024-08-30 16:01:57 +0000

    Fetched current temperature

--- umbrella.rb
+++ umbrella.rb
@@ -11,6 +11,8 @@ user_location = gets.chomp
 
 pp user_location
 
+#Using Google Maps API, fetch the latitude and longitude
+
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
 resp = HTTP.get(maps_url)
@@ -29,3 +31,25 @@ loc = geo.fetch("location")
 
 pp latitude = loc.fetch("lat")
 pp longitude = loc.fetch("lng")
+
+
+# Take the lat/lng
+# Assemble the correct URL for the Pirate Weather API
+# Get it, parse it, and dig out current temperature
+
+
+# Assemble the Pirate Weather API URL
+
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{latitude},#{longitude}"
+# pirate_weather_url = "https://api.pirateweather.net/forecast/#{ENV.fetch("PIRATE_WEATHER_KEY")}/#{latitude},#{longitude}?units=si"
+
+# Make a request to the Pirate API and parse the response
+
+weather_resp = HTTP.get(pirate_weather_url)
+weather_data = JSON.parse(weather_resp.to_s)
+
+# Dig out current temperature
+
+current_temp = weather_data.dig("currently", "temperature")
+
+pp "The current temperature at #{user_location} is #{current_temp}°F"
